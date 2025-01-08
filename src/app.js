4e9b033d71187406873279363f4b79a25f98b6a5
Saray Rodríguez <rodriguezsaray602@gmail.com>
2025-01-08 17:08:56 +0000

    no esta listo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,38 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const simbolos = ["♦", "♥", "♠", "♣"];
+  const numeros = [
+    "A",
+    ...Array.from({ length: 9 }, (_, i) => i + 2),
+    "J",
+    "Q",
+    "K"
+  ];
+
+  const btnGenerar = document.getElementById("btnGenrar");
+  const simbolTop = document.querySelectorAll("#simbol")[0];
+  const simbolBottom = document.querySelectorAll("#simbol")[1];
+  const numberElement = document.getElementById("number");
+
+  function generarCarta() {
+    const simboloAleatorio =
+      simbolos[Math.floor(Math.random() * simbolos.length)];
+    const numeroAleatorio = numeros[Math.floor(Math.random() * numeros.length)];
+
+    simbolTop.textContent = simboloAleatorio;
+    simbolBottom.textContent = simboloAleatorio;
+    numberElement.textContent = numeroAleatorio;
+
+    const color =
+      simboloAleatorio === "♦" || simboloAleatorio === "♥" ? "red" : "black";
+    simbolTop.style.color = color;
+    simbolBottom.style.color = color;
+    numberElement.style.color = color;
+  }
+
+  btnGenerar.addEventListener("click", generarCarta);
+
+  generarCarta();
 };
